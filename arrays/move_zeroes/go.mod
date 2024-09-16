8b045c72beaeb3c0534c168fc7793d78d8fb8cae
welder87 <25900160+welder87@users.noreply.github.com>
2024-09-16 23:47:22 +0500

    Move Zeroes

---
+++ arrays/move_zeroes/go.mod
@@ -0,0 +1,3 @@
+module move_zeroes
+
+go 1.22.3
---
+++ arrays/move_zeroes/main.go
@@ -0,0 +1,23 @@
+package movezeroes
+
+func MoveZeroes(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+	i := 0
+	j := 1
+	for i < (len(nums)-1) && j < len(nums) {
+		if nums[i] != 0 && nums[j] != 0 {
+			i++
+			j++
+		} else if nums[i] == 0 && nums[j] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+			j++
+		} else if nums[i] == 0 && nums[j] == 0 {
+			j++
+		} else {
+			i++
+		}
+	}
+}
