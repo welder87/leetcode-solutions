d8cd759d161bb4806f0a62ab5d83ade754569957
welder87 <25900160+welder87@users.noreply.github.com>
2024-09-19 23:42:49 +0500

    Intersection Two Arrays 2 var 2

--- arrays/intersection_two_arrays2/main.go
+++ arrays/intersection_two_arrays2/main.go
@@ -2,6 +2,33 @@ package intersectiontwoarrays2
 
 func Intersect(nums1 []int, nums2 []int) []int {
 	numsWithCount1 := make(map[int]int)
+	for _, num := range nums1 {
+		if _, ok := numsWithCount1[num]; ok {
+			numsWithCount1[num]++
+		} else {
+			numsWithCount1[num] = 1
+		}
+	}
+	vectorLength := len(nums1)
+	if len(nums1) > len(nums2) {
+		vectorLength = len(nums2)
+	}
+	result := make([]int, 0, vectorLength)
+	for _, num := range nums2 {
+		if count, ok := numsWithCount1[num]; ok {
+			if count <= 1 {
+				delete(numsWithCount1, num)
+			} else {
+				numsWithCount1[num]--
+			}
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func Intersect1(nums1 []int, nums2 []int) []int {
+	numsWithCount1 := make(map[int]int)
 	for _, val := range nums1 {
 		if _, ok := numsWithCount1[val]; ok {
 			numsWithCount1[val]++
