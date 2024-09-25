835b77ab71ffa42d0533201371959a91cf12cec3
welder87 <25900160+welder87@users.noreply.github.com>
2024-09-25 23:38:02 +0500

    Minimum Average var 2

--- minimum_average/main.go
+++ minimum_average/main.go
@@ -2,11 +2,10 @@ package minimumaverage
 
 import (
 	"slices"
-	"sort"
 )
 
 func MinimumAverage(nums []int) float64 {
-	sort.Ints(nums)
+	nums = quickSort(nums)
 	average := make([]float64, 0, len(nums))
 	i := 0
 	j := len(nums) - 1
@@ -16,4 +15,31 @@ func MinimumAverage(nums []int) float64 {
 		j--
 	}
 	return slices.Min(average)
+}
+
+func quickSort(arr []int) []int {
+	lengthOfVector := len(arr)
+	if lengthOfVector <= 1 {
+		return arr
+	}
+	middleIndex := lengthOfVector / 2
+	pivot := arr[middleIndex]
+	new := make([]int, 0, lengthOfVector)
+	left := make([]int, 0, lengthOfVector)
+	middle := make([]int, 0, lengthOfVector)
+	right := make([]int, 0, lengthOfVector)
+
+	for _, val := range arr {
+		if val < pivot {
+			left = append(left, val)
+		} else if val == pivot {
+			middle = append(middle, val)
+		} else {
+			right = append(right, val)
+		}
+	}
+	new = append(new, quickSort(left)...)
+	new = append(new, middle...)
+	new = append(new, quickSort(right)...)
+	return new
 }
