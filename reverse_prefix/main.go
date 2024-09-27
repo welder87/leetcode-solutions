6d4f68944550f826963526601513d4d3fbf18d47
welder87 <25900160+welder87@users.noreply.github.com>
2024-09-28 00:11:26 +0500

    Reverse Prefix

---
+++ reverse_prefix/main.go
@@ -0,0 +1,34 @@
+package reverseprefix
+
+func ReversePrefix(word string, ch byte) string {
+	idx := -1
+	for i := 0; i < len(word); i++ {
+		if word[i] == ch {
+			idx = i
+			break
+		}
+	}
+	if idx == -1 || idx == len(word) {
+		return word
+	}
+	newString := make([]byte, len(word))
+	i := idx
+	j := idx + 1
+	n := 0
+	for {
+		leftOk := i >= 0
+		rightOk := j < len(word)
+		if !leftOk && !rightOk {
+			return string(newString)
+		}
+		if leftOk {
+			newString[n] = word[i]
+			i--
+			n++
+		}
+		if rightOk {
+			newString[j] = word[j]
+			j++
+		}
+	}
+}
