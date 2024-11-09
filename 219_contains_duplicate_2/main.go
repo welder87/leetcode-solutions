21ed778a27b202cdf5a33dd6b10e057d888082c0
welder87 <25900160+welder87@users.noreply.github.com>
2024-11-10 00:06:08 +0500

    219. Contains Duplicate 2. Slow Solution

--- 219_contains_duplicate_2/main.go
+++ 219_contains_duplicate_2/main.go
@@ -12,12 +12,28 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 		if len(idxs) <= 1 {
 			continue
 		}
-
-		for i := 0; i < len(idxs); i++ {
+		lastVal := len(nums) - 1
+		for i := 0; i < lastVal; i++ {
 			for j := i + 1; j < len(idxs); j++ {
 				if (idxs[j] - idxs[i]) <= k {
 					return true
 				}
+			}
+		}
+	}
+	return false
+}
+
+// very slow
+func containsNearbyDuplicateV1(nums []int, k int) bool {
+	if len(nums) == 0 || len(nums) == 1 {
+		return false
+	}
+	lastVal := len(nums) - 1
+	for i := 0; i < lastVal; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] && j-i <= k {
+				return true
 			}
 		}
 	}
