14d8096adfe86d85f3a6084af9fa2be7d389dc09
welder87 <25900160+welder87@users.noreply.github.com>
2024-11-13 00:53:36 +0500

    33. Search in Rotated Sorted Array. Sketch

---
+++ 33_search_in_rotated_sorted_array/main.go
@@ -0,0 +1,14 @@
+package problem33
+
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	if len(nums) == 1 {
+		if nums[0] == target {
+			return 0
+		}
+		return -1
+	}
+	return -1
+}
---
+++ 33_search_in_rotated_sorted_array/main_test.go
@@ -0,0 +1,45 @@
+package problem33
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	testCases := []struct {
+		nums   []int
+		target int
+		ans    int
+	}{
+		{
+			nums:   []int{4, 5, 6, 7, 0, 1, 2},
+			target: 0,
+			ans:    4,
+		},
+		{
+			nums:   []int{4, 5, 6, 7, 0, 1, 2},
+			target: 3,
+			ans:    -1,
+		},
+		{
+			nums:   []int{1},
+			target: 1,
+			ans:    0,
+		},
+		{
+			nums:   []int{},
+			target: 25,
+			ans:    -1,
+		},
+	}
+	for _, testCase := range testCases {
+		testName := fmt.Sprintf("%v %v", testCase.nums, testCase.target)
+		t.Run(testName, func(t *testing.T) {
+			ans := search(testCase.nums, testCase.target)
+			if !reflect.DeepEqual(testCase.ans, ans) {
+				t.Errorf("got %v, want %v", ans, testCase.ans)
+			}
+		})
+	}
+}
