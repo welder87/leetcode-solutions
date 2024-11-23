95eaad940469403fc3abb9fd2fe8176d035e6e99
welder87 <25900160+welder87@users.noreply.github.com>
2024-11-23 23:49:34 +0500

    724. Find Pivot Index. Prefix Sum

---
+++ 724_find_pivot_index/main.go
@@ -0,0 +1,16 @@
+package problem724
+
+func pivotIndex(nums []int) int {
+	prefixSum := make([]int, len(nums)+1)
+	prefixSum[0] = 0
+	for idx := 0; idx < len(nums); idx++ {
+		prefixSum[idx+1] = prefixSum[idx] + nums[idx]
+	}
+	lastIndex := len(prefixSum) - 1
+	for idx := 1; idx < len(prefixSum); idx++ {
+		if prefixSum[idx-1]-prefixSum[0] == prefixSum[lastIndex]-prefixSum[idx] {
+			return idx - 1
+		}
+	}
+	return -1
+}
---
+++ 724_find_pivot_index/main_test.go
@@ -0,0 +1,51 @@
+package problem724
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPivotIndex(t *testing.T) {
+	testCases := []struct {
+		nums []int
+		ans  int
+	}{
+		{
+			nums: []int{1, 2, 3, 4, 5},
+			ans:  -1,
+		},
+		{
+			nums: []int{1, 7, 3, 6, 5, 6},
+			ans:  3,
+		},
+		{
+			nums: []int{1, 2, 3},
+			ans:  -1,
+		},
+		{
+			nums: []int{2, 1, -1},
+			ans:  0,
+		},
+		{
+			nums: []int{1},
+			ans:  0,
+		},
+		{
+			nums: []int{-1, 1, 2},
+			ans:  2,
+		},
+		{
+			nums: []int{12, -3, 10, 5, -46, -31},
+			ans:  -1,
+		},
+	}
+	for _, testCase := range testCases {
+		testName := fmt.Sprintf("%v", testCase.nums)
+		t.Run(testName, func(t *testing.T) {
+			ans := pivotIndex(testCase.nums)
+			if testCase.ans != ans {
+				t.Errorf("got %v, want %v", ans, testCase.ans)
+			}
+		})
+	}
+}
