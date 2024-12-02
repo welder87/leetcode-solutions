e5e4d76d2a51bb65751b7b1981958e1d38541484
welder87 <25900160+welder87@users.noreply.github.com>
2024-12-02 23:27:19 +0500

    744. Find Smallest Letter Greater Than Target. O(LogN)

--- 744_find_smallest_letter_greater_than_target/main.go
+++ 744_find_smallest_letter_greater_than_target/main.go
@@ -9,3 +9,20 @@ func nextGreatestLetter(letters []byte, target byte) byte {
 	}
 	return letters[0]
 }
+
+func nextGreatestLetterV1(letters []byte, target byte) byte {
+	if target < letters[0] || target >= letters[len(letters)-1] {
+		return letters[0]
+	}
+	left := 0
+	right := len(letters)
+	for left < right {
+		mid := left + (right-left)/2
+		if letters[mid] > target {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return letters[left%len(letters)]
+}
--- 744_find_smallest_letter_greater_than_target/main_test.go
+++ 744_find_smallest_letter_greater_than_target/main_test.go
@@ -6,6 +6,14 @@ import (
 )
 
 func TestNextGreatestLetter(t *testing.T) {
+	testNextGreatestLetter(t, nextGreatestLetter)
+}
+
+func TestNextGreatestLetterV1(t *testing.T) {
+	testNextGreatestLetter(t, nextGreatestLetterV1)
+}
+
+func testNextGreatestLetter(t *testing.T, function fn) {
 	testCases := []struct {
 		letters []byte
 		target  byte
@@ -32,18 +40,55 @@ func TestNextGreatestLetter(t *testing.T) {
 			ans:     byte('x'),
 		},
 		{
+			letters: []byte("xy"),
+			target:  byte('y'),
+			ans:     byte('x'),
+		},
+		{
+			letters: []byte("xy"),
+			target:  byte('x'),
+			ans:     byte('y'),
+		},
+		{
 			letters: []byte("abd"),
 			target:  byte('c'),
 			ans:     byte('d'),
 		},
+		{
+			letters: []byte("abd"),
+			target:  byte('j'),
+			ans:     byte('a'),
+		},
+		{
+			letters: []byte("abcdefjhkm"),
+			target:  byte('b'),
+			ans:     byte('c'),
+		},
+		{
+			letters: []byte("abcdefjhkmn"),
+			target:  byte('b'),
+			ans:     byte('c'),
+		},
+		{
+			letters: []byte("acdefjhkmn"),
+			target:  byte('b'),
+			ans:     byte('c'),
+		},
+		{
+			letters: []byte("ajhkmnoprs"),
+			target:  byte('b'),
+			ans:     byte('j'),
+		},
 	}
 	for _, testCase := range testCases {
-		testName := fmt.Sprintf("%v %v", testCase.letters, testCase.target)
+		testName := fmt.Sprintf("%v %v", string(testCase.letters), string(testCase.target))
 		t.Run(testName, func(t *testing.T) {
-			ans := nextGreatestLetter(testCase.letters, testCase.target)
+			ans := function(testCase.letters, testCase.target)
 			if testCase.ans != ans {
 				t.Errorf("got %v, want %v", ans, testCase.ans)
 			}
 		})
 	}
 }
+
+type fn func(letters []byte, target byte) byte
