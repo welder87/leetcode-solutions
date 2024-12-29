ff4c83d31229c3cfc2e1a63eb0a76f8ca642c46d
welder87 <25900160+welder87@users.noreply.github.com>
2024-12-29 15:27:57 +0500

    2485. Find the Pivot Integer. O(n) + O(n)

---
+++ 2485_find_the_pivot_integer/main.go
@@ -0,0 +1,17 @@
+package problem2485
+
+func pivotInteger(n int) int {
+	prefixSum := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		prefixSum[i] = prefixSum[i-1] + i
+	}
+
+	for i := 1; i <= n; i++ {
+		sum1 := prefixSum[i] - prefixSum[0]
+		sum2 := prefixSum[n] - prefixSum[i-1]
+		if sum1 == sum2 {
+			return i
+		}
+	}
+	return -1
+}
---
+++ 2485_find_the_pivot_integer/main_test.go
@@ -0,0 +1,37 @@
+package problem2485
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPivotInteger(t *testing.T) {
+	testPivotInteger(t, pivotInteger)
+}
+
+func testPivotInteger(t *testing.T, function fn) {
+	testCases := []struct {
+		n   int
+		ans int
+	}{
+		{n: 8, ans: 6},
+		{n: 1, ans: 1},
+		{n: 4, ans: -1},
+		{n: 2, ans: -1},
+		{n: 3, ans: -1},
+		{n: 5, ans: -1},
+		{n: 6, ans: -1},
+		{n: 7, ans: -1},
+	}
+	for _, testCase := range testCases {
+		testName := fmt.Sprintf("%v", testCase.n)
+		t.Run(testName, func(t *testing.T) {
+			ans := function(testCase.n)
+			if ans != testCase.ans {
+				t.Errorf("got %v, want %v", ans, testCase.ans)
+			}
+		})
+	}
+}
+
+type fn func(int) int
