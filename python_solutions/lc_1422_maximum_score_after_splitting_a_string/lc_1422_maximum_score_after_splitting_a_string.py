cccfcc4cbb95a38980fdfc1bbb2a94ae2bb9a3eb
welder87 <25900160+welder87@users.noreply.github.com>
2025-01-21 17:57:02 +0500

    1422. Maximum Score After Splitting a String. Fixed comparison.

--- python_solutions/lc_1422_maximum_score_after_splitting_a_string/lc_1422_maximum_score_after_splitting_a_string.py
+++ python_solutions/lc_1422_maximum_score_after_splitting_a_string/lc_1422_maximum_score_after_splitting_a_string.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        left = 1 if int(s[0]) == 0 else 0
+        left = 1 if s[0] == "0" else 0
         right = 0
         for i in range(1, len(s)):
-            if int(s[i]) == 1:
+            if s[i] == "1":
                 right += 1
         mx = left + right
         for i in range(1, len(s) - 1):
-            if (val := int(s[i])) == 1:
+            if s[i] == "1":
                 right -= 1
-            elif val == 0:
+            elif s[i] == "0":
                 left += 1
             if (sm := left + right) > mx:
                 mx = sm
