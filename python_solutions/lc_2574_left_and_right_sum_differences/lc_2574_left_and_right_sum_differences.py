8a9106df42488f034286b4eafc089dbef9be8d0e
welder87 <25900160+welder87@users.noreply.github.com>
2025-01-21 13:06:50 +0500

    2574. Left and Right Sum Differences. O(2n) + O(n)

--- python_solutions/lc_2574_left_and_right_sum_differences/lc_2574_left_and_right_sum_differences.py
+++ python_solutions/lc_2574_left_and_right_sum_differences/lc_2574_left_and_right_sum_differences.py
@@ -11,3 +11,15 @@ class Solution:
         for idx, (elem1, elem2) in enumerate(zip(left, right)):
             result[idx] = abs(elem1 - elem2)
         return result
+
+    def leftRightDifferenceV1(self, nums: list[int]) -> list[int]:
+        length = len(nums)
+        last_index = length - 1
+        result = [0] * length
+        for i in range(last_index):
+            result[i + 1] = result[i] + nums[i]
+        prev = 0
+        for i in range(last_index, -1, -1):
+            result[i] = abs(prev - result[i])
+            prev += nums[i]
+        return result
--- python_solutions/lc_2574_left_and_right_sum_differences/test_lc_2574_left_and_right_sum_differences.py
+++ python_solutions/lc_2574_left_and_right_sum_differences/test_lc_2574_left_and_right_sum_differences.py
@@ -19,6 +19,11 @@ def test_success_v0(nums: list[int], ans: list[int], solution: Solution):
     assert ans == solution.leftRightDifference(nums)
 
 
+@pytest.mark.parametrize(("nums", "ans"), test_cases)
+def test_success_v1(nums: list[int], ans: list[int], solution: Solution):
+    assert ans == solution.leftRightDifferenceV1(nums)
+
+
 @pytest.fixture
 def solution() -> Solution:
     return Solution()
