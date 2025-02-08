513c15fd759e55e750426461d2e54ab72ee5eb81
welder87 <25900160+welder87@users.noreply.github.com>
2025-02-08 22:33:13 +0500

    268. Missing Number. Golang.

--- golang_solutions/268_missing_number/main.go
+++ golang_solutions/268_missing_number/main.go
@@ -1,26 +1,80 @@
 package problem268
 
+import "sort"
+
+// Time complexity: O(n^2). Space complexity: O(m).
 func missingNumber(nums []int) int {
-	minNum := -1
-	var maxNum, sum int
-	for _, num := range nums {
-		if num > maxNum {
-			maxNum = num
+	maxIdx := len(nums) + 1
+	for i := 0; i < maxIdx; i++ {
+		isFound := false
+		for j := 0; j < len(nums); j++ {
+			if i == nums[j] {
+				isFound = true
+				break
+			}
 		}
-		if minNum == -1 || num < minNum {
-			minNum = num
+		if !isFound {
+			return i
 		}
-		sum += num
 	}
-	fullSum := 0
-	for i := minNum; i <= maxNum; i++ {
-		fullSum += i
+	return -1
+}
+
+// Time complexity: O(2n). Space complexity: O(n).
+func missingNumberV1(nums []int) int {
+	uniqueNums := make(map[int]struct{}, len(nums))
+	for i := 0; i < len(nums); i++ {
+		uniqueNums[nums[i]] = struct{}{}
 	}
-	if fullSum == sum {
-		if minNum != 0 {
-			return 0
+	maxIdx := len(nums) + 1
+	for i := 0; i < maxIdx; i++ {
+		if _, ok := uniqueNums[i]; !ok {
+			return i
 		}
-		return maxNum + 1
 	}
-	return fullSum - sum
+	return -1
+}
+
+// Time complexity: O(n log n + n + 1). Space complexity: O(n log n).
+func missingNumberV2(nums []int) int {
+	sort.Ints(nums)
+	maxIdx := len(nums) + 1
+	for i := 0; i < maxIdx; i++ {
+		if len(nums) == i || i != nums[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+// Time complexity: O(2n). Space complexity: O(1).
+func missingNumberV3(nums []int) int {
+	current := 0
+	for i := 0; i < len(nums); i++ {
+		current += nums[i]
+	}
+	reference := 0
+	maxIdx := len(nums) + 1
+	for i := 0; i < maxIdx; i++ {
+		reference += i
+	}
+	if val := reference - current; val < 0 {
+		return val * -1
+	} else {
+		return val
+	}
+}
+
+// Time complexity: O(2n). Space complexity: O(1).
+func missingNumberV4(nums []int) int {
+	current := 0
+	for i := 0; i < len(nums); i++ {
+		current ^= nums[i]
+	}
+	reference := 0
+	maxIdx := len(nums) + 1
+	for i := 0; i < maxIdx; i++ {
+		reference ^= i
+	}
+	return current ^ reference
 }
--- golang_solutions/268_missing_number/main_test.go
+++ golang_solutions/268_missing_number/main_test.go
@@ -5,44 +5,54 @@ import (
 	"testing"
 )
 
+type fn func([]int) int
+
 func TestMissingNumber(t *testing.T) {
+	testMissingNumber(t, missingNumber)
+}
+
+func TestMissingNumberV1(t *testing.T) {
+	testMissingNumber(t, missingNumberV1)
+}
+
+func TestMissingNumberV2(t *testing.T) {
+	testMissingNumber(t, missingNumberV2)
+}
+
+func TestMissingNumberV3(t *testing.T) {
+	testMissingNumber(t, missingNumberV3)
+}
+
+func TestMissingNumberV4(t *testing.T) {
+	testMissingNumber(t, missingNumberV4)
+}
+
+func testMissingNumber(t *testing.T, function fn) {
 	testCases := []struct {
 		nums []int
 		ans  int
 	}{
-		{
-			nums: []int{3, 0, 1},
-			ans:  2,
-		},
-		{
-			nums: []int{0, 1},
-			ans:  2,
-		},
-		{
-			nums: []int{1},
-			ans:  0,
-		},
-		{
-			nums: []int{0},
-			ans:  1,
-		},
-		{
-			nums: []int{9, 6, 4, 2, 3, 5, 7, 0, 1},
-			ans:  8,
-		},
-		{
-			nums: []int{9, 6, 4, 2, 3, 5, 7, 8, 1},
-			ans:  0,
-		},
-		{
-			nums: []int{9, 6, 4, 2, 3, 5, 7, 8, 0, 1},
-			ans:  10,
-		},
+		// preset cases
+		{[]int{3, 0, 1}, 2},
+		{[]int{0, 1}, 2},
+		{[]int{9, 6, 4, 2, 3, 5, 7, 0, 1}, 8},
+		{[]int{1, 8, 4, 6, 2, 0, 9, 7, 5}, 3},
+		// common cases
+		{[]int{9, 6, 4, 2, 3, 5, 7, 8, 1}, 0},
+		{[]int{9, 6, 4, 2, 3, 5, 7, 8, 0, 1}, 10},
+		{[]int{2, 0, 1}, 3},
+		{[]int{3, 0, 1, 5, 4}, 2},
+		{[]int{3, 0, 4, 5, 2}, 1},
+		{[]int{2, 3, 4, 5, 1}, 0},
+		{[]int{2, 3, 0, 5, 1}, 4},
+		// corner cases
+		{[]int{1}, 0},
+		{[]int{0}, 1},
 	}
 	for _, testCase := range testCases {
 		testName := fmt.Sprintf("%v", testCase.nums)
 		t.Run(testName, func(t *testing.T) {
-			ans := missingNumber(testCase.nums)
+			ans := function(testCase.nums)
 			if testCase.ans != ans {
 				t.Errorf("got %v, want %v", ans, testCase.ans)
 			}
