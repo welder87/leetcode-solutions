3a7a0f77589897a8930ac61cc04408ce2a545db5
welder87 <25900160+welder87@users.noreply.github.com>
2025-02-15 15:29:17 +0500

    713. Subarray Product Less Than K. Python. Simplification

--- python_solutions/lc_713_subarray_product_less_than_k/lc_713_subarray_product_less_than_k.py
+++ python_solutions/lc_713_subarray_product_less_than_k/lc_713_subarray_product_less_than_k.py
@@ -1,16 +1,13 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: list[int], k: int) -> int:
-        if k == 0:
+        if k <= 1:
             return 0
-        ans, prod, left = 0, 1, 0
+        ans = 0
+        prod, left = 1, 0
         for right, num in enumerate(nums):
             prod *= num
-            while prod >= k and left < len(nums):
+            while prod >= k:
                 prod //= nums[left]
-                if prod < 1:
-                    prod = 1
                 left += 1
-            if left >= len(nums):
-                break
             ans += right - left + 1
         return ans
