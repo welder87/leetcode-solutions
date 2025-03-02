cfad6ed94629acc3fd1743d3f3517315e1266ce2
welder87 <25900160+welder87@users.noreply.github.com>
2025-03-02 23:22:56 +0500

    1394. Find Lucky Integer in an Array. Python. Time complexity: O(2n + m). Space complexity: O(m).

--- python_solutions/lc_1394_find_lucky_integer_in_an_array/lc_1394_find_lucky_integer_in_an_array.py
+++ python_solutions/lc_1394_find_lucky_integer_in_an_array/lc_1394_find_lucky_integer_in_an_array.py
@@ -9,3 +9,14 @@ class Solution:
             if num == cnt and largest_lucky < num:
                 largest_lucky = num
         return largest_lucky
+
+    def findLuckyV1(self, arr: list[int]) -> int:
+        # Time complexity: O(2n + m). Space complexity: O(m).
+        counter = [0] * (max(arr) + 1)
+        for num in arr:
+            counter[num] += 1
+        largest_lucky = -1
+        for num in range(1, len(counter)):
+            if num == counter[num] and largest_lucky < num:
+                largest_lucky = num
+        return largest_lucky
