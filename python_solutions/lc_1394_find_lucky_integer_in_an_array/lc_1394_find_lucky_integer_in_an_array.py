4cb3c88fdb6272a303d53ccc8ac3a524068cdf17
welder87 <25900160+welder87@users.noreply.github.com>
2025-03-02 23:37:46 +0500

    1394. Find Lucky Integer in an Array. Python. Time complexity: O(n log n + n). Space complexity: O(n).

--- python_solutions/lc_1394_find_lucky_integer_in_an_array/lc_1394_find_lucky_integer_in_an_array.py
+++ python_solutions/lc_1394_find_lucky_integer_in_an_array/lc_1394_find_lucky_integer_in_an_array.py
@@ -20,3 +20,22 @@ class Solution:
             if num == counter[num] and largest_lucky < num:
                 largest_lucky = num
         return largest_lucky
+
+    def findLuckyV2(self, arr: list[int]) -> int:
+        # Time complexity: O(n log n + n). Space complexity: O(n).
+        if len(arr) == 1:
+            if arr[0] == 1:
+                return 1
+            return -1
+        arr.sort()
+        length = len(arr)
+        i = length - 1
+        largest_lucky = -1
+        while i >= 0:
+            counter, num = 0, arr[i]
+            while i >= 0 and num == arr[i]:
+                counter += 1
+                i -= 1
+            if num == counter and largest_lucky < num:
+                largest_lucky = num
+        return largest_lucky
--- python_solutions/lc_1394_find_lucky_integer_in_an_array/test_lc_1394_find_lucky_integer_in_an_array.py
+++ python_solutions/lc_1394_find_lucky_integer_in_an_array/test_lc_1394_find_lucky_integer_in_an_array.py
@@ -26,6 +26,11 @@ def test_success_v1(nums: list[int], ans: int, solution: Solution):
     assert solution.findLuckyV1(nums) == ans
 
 
+@pytest.mark.parametrize(("nums", "ans"), test_cases)
+def test_success_v2(nums: list[int], ans: int, solution: Solution):
+    assert solution.findLuckyV2(nums) == ans
+
+
 @pytest.fixture
 def solution() -> Solution:
     return Solution()
