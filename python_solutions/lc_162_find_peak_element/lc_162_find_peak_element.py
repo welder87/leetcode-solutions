212efbac8bef3907cd51c2ad1b2f37b464ebf320
welder87 <25900160+welder87@users.noreply.github.com>
2025-03-14 20:24:58 +0500

    162. Find Peak Element. Python. Time complexity: O(log n). Space complexity: O(1).

--- python_solutions/lc_162_find_peak_element/lc_162_find_peak_element.py
+++ python_solutions/lc_162_find_peak_element/lc_162_find_peak_element.py
@@ -14,3 +14,23 @@ class Solution:
             ):
                 return i + 1
         return -1
+
+    def findPeakElementV1(self, nums: list[int]) -> int:
+        # Time complexity: O(log n). Space complexity: O(1).
+        if len(nums) == 1:
+            return 0
+
+        left, right = -1, len(nums)
+        while 1:
+            mid = left + (right - left) // 2
+            if (
+                (mid - 1 == -1 and nums[mid] > nums[mid + 1])
+                or (mid + 1 == len(nums) and nums[mid - 1] < nums[mid])
+                or (nums[mid - 1] < nums[mid] > nums[mid + 1])
+            ):
+                return mid
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid
+        return -1
--- python_solutions/lc_162_find_peak_element/test_lc_162_find_peak_element.py
+++ python_solutions/lc_162_find_peak_element/test_lc_162_find_peak_element.py
@@ -9,17 +9,25 @@ test_cases = (
     ([1, 2, 1, 3, 5, 6, 4], {1, 5}),
     # common cases
     ([7, 6, 4, 5, 6, 7, 8], {0, 6}),
+    ([-7, -6, -8, 5, 8, 6, 3, 9, 8], {1, 4, 7}),
     ([7, 8, 4], {1}),
+    ([7, 6, 4, 3, 2, -1, -5], {0}),
     # corner cases
     ([7, 6, 4], {0}),
     ([-1], {0}),
     ([-1, 0], {1}),
+    ([0, -1], {0}),
 )
 
 
 @pytest.mark.parametrize(("nums", "ans"), test_cases)
 def test_success_v0(nums: list[int], ans: int, solution: Solution):
     assert solution.findPeakElement(nums) in ans
+
+
+@pytest.mark.parametrize(("nums", "ans"), test_cases)
+def test_success_v1(nums: list[int], ans: int, solution: Solution):
+    assert solution.findPeakElementV1(nums) in ans
 
 
 @pytest.fixture
