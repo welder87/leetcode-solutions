014d6a92370797c43f85c9e835fec3ea6f23ffce
welder87 <25900160+welder87@users.noreply.github.com>
2025-03-21 00:58:15 +0500

    153. Find Minimum in Rotated Sorted Array. Golang. Time complexity: O(log n). Space complexity: O(1).

--- golang_solutions/153_find_minimum_in_rotated_sorted_array/main.go
+++ golang_solutions/153_find_minimum_in_rotated_sorted_array/main.go
@@ -1,8 +1,8 @@
 package problem153
 
+// Time complexity: O(log n). Space complexity: O(1).
 func findMin(nums []int) int {
-	left := 0
-	right := len(nums) - 1
+	left, right := 0, len(nums)-1
 	ans := -1
 	for left <= right {
 		middle := left + (right-left)>>1
@@ -14,4 +14,18 @@ func findMin(nums []int) int {
 		}
 	}
 	return nums[ans]
+}
+
+// Time complexity: O(log n). Space complexity: O(1).
+func findMinV1(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := left + (right-left)>>1
+		if nums[mid] > nums[len(nums)-1] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return nums[left]
 }
--- golang_solutions/153_find_minimum_in_rotated_sorted_array/main_test.go
+++ golang_solutions/153_find_minimum_in_rotated_sorted_array/main_test.go
@@ -5,40 +5,42 @@ import (
 	"testing"
 )
 
+type fn func([]int) int
+
 func TestFindMin(t *testing.T) {
+	testFindMin(t, findMin)
+}
+
+func TestFindMinV1(t *testing.T) {
+	testFindMin(t, findMinV1)
+}
+
+func testFindMin(t *testing.T, function fn) {
 	testCases := []struct {
 		nums []int
 		ans  int
 	}{
-		{
-			nums: []int{3, 4, 5, 1, 2},
-			ans:  1,
-		},
-		{
-			nums: []int{4, 5, 6, 7, 0, 1, 2},
-			ans:  0,
-		},
-		{
-			nums: []int{11, 13, 15, 17},
-			ans:  11,
-		},
-		{
-			nums: []int{4, 6, 7, 8, 9, 0, 1, 2},
-			ans:  0,
-		},
-		{
-			nums: []int{17, 11, 13, 15},
-			ans:  11,
-		},
-		{
-			nums: []int{1},
-			ans:  1,
-		},
+		// preset cases
+		{[]int{3, 4, 5, 1, 2}, 1},
+		{[]int{4, 5, 6, 7, 0, 1, 2}, 0},
+		{[]int{11, 13, 15, 17}, 11},
+		// common cases
+		{[]int{4, 6, 7, 8, 9, 0, 1, 2}, 0},
+		{[]int{17, 11, 13, 15}, 11},
+		{[]int{2, 3, 4, 5, 1}, 1},
+		{[]int{4, 5, 1, 2, 3}, 1},
+		{[]int{5, 1, 2, 3, 4}, 1},
+		{[]int{5, 8, -7, -3, 0}, -7},
+		// corner cases
+		{[]int{1, 0}, 0},
+		{[]int{3}, 3},
+		{[]int{-3}, -3},
+		{[]int{0}, 0},
 	}
 	for _, testCase := range testCases {
 		testName := fmt.Sprintf("%v", testCase.nums)
 		t.Run(testName, func(t *testing.T) {
-			ans := findMin(testCase.nums)
+			ans := function(testCase.nums)
 			if testCase.ans != ans {
 				t.Errorf("got %v, want %v", ans, testCase.ans)
 			}
