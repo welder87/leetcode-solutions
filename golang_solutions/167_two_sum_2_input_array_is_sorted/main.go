29aead0289e0f9db685fd323e83a1851ffebdc31
welder87 <25900160+welder87@users.noreply.github.com>
2025-04-21 23:43:53 +0500

    167. Two Sum II - Input Array Is Sorted. Golang. Time complexity: O(n log n). Space complexity: O(1).

---
+++ golang_solutions/167_two_sum_2_input_array_is_sorted/main.go
@@ -0,0 +1,43 @@
+package problem167
+
+// Time complexity: O(n log n). Space complexity: O(1).
+// modified itmo
+func twoSum(numbers []int, target int) []int {
+	for i := 0; i < len(numbers); i++ {
+		num := target - numbers[i]
+		left, right := -1, len(numbers)
+		for right > left+1 {
+			mid := left + (right-left)>>1
+			if numbers[mid] == num && mid != i {
+				return []int{i + 1, mid + 1}
+			}
+			if numbers[mid] <= num {
+				left = mid
+			} else {
+				right = mid
+			}
+		}
+	}
+	return []int{}
+}
+
+// Time complexity: O(n log n). Space complexity: O(1).
+// modified leetcode
+func twoSumV1(numbers []int, target int) []int {
+	for i := 0; i < len(numbers); i++ {
+		num := target - numbers[i]
+		left, right := 0, len(numbers)-1
+		for left < right {
+			mid := left + (right-left)>>1
+			if numbers[mid] == num && mid != i {
+				return []int{min(mid, i) + 1, max(mid, i) + 1}
+			}
+			if numbers[mid] <= num {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		}
+	}
+	return []int{}
+}
---
+++ golang_solutions/167_two_sum_2_input_array_is_sorted/main_test.go
@@ -0,0 +1,46 @@
+package problem167
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type fn func([]int, int) []int
+
+func TestTwoSum(t *testing.T) {
+	testTwoSum(t, twoSum)
+}
+
+func TestTwoSumV1(t *testing.T) {
+	testTwoSum(t, twoSumV1)
+}
+
+func testTwoSum(t *testing.T, function fn) {
+	testCases := []struct {
+		nums   []int
+		target int
+		ans    []int
+	}{
+		// preset cases
+		{[]int{2, 7, 11, 15}, 9, []int{1, 2}},
+		{[]int{2, 3, 4}, 6, []int{1, 3}},
+		{[]int{-1, 0}, -1, []int{1, 2}},
+		// common cases
+		{[]int{-15, -3, 0, 11, 15}, 8, []int{2, 4}},
+		{[]int{-15, -3, 0, 11, 15}, 0, []int{1, 5}},
+		{[]int{-15, -3, 0, 11, 15}, 11, []int{3, 4}},
+		// corner cases
+		{[]int{0, 0}, 0, []int{1, 2}},
+		{[]int{-1, -1}, -2, []int{1, 2}},
+	}
+	for _, testCase := range testCases {
+		testName := fmt.Sprintf("%v %v", testCase.nums, testCase.target)
+		t.Run(testName, func(t *testing.T) {
+			ans := function(testCase.nums, testCase.target)
+			if !reflect.DeepEqual(ans, testCase.ans) {
+				t.Errorf("got %v, want %v", ans, testCase.ans)
+			}
+		})
+	}
+}
