d799302393fc14aa9d0e3ecdba7e583030c780aa
welder87 <25900160+welder87@users.noreply.github.com>
2025-06-27 00:24:00 +0500

    1539. Kth Missing Positive Number. Python. Time complexity: O(n log n). Space complexity: O(1).

---
+++ python_solutions/lc_1539_kth_missing_positive_number/lc_1539_kth_missing_positive_number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findKthPositive(self, arr: list[int], k: int) -> int:
+        # Time complexity: O(n log n). Space complexity: O(1).
+        # O(arr[−1]∗K∗log(len(arr)))
+        min_num, max_num = 1, arr[-1] * k + 1
+        counter = 0
+        num = -1
+        for num in range(min_num, max_num + 1, 1):
+            if not self.bs(arr, num):
+                counter += 1
+            if counter == k:
+                break
+        return num
+
+    @staticmethod
+    def bs(nums: list[int], target: int) -> bool:
+        left, right = 0, len(nums) - 1
+        while left + 1 < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid
+            else:
+                right = mid
+        if nums[left] == target or nums[right] == target:
+            return True
+        return False
---
+++ python_solutions/lc_1539_kth_missing_positive_number/test_lc_1539_kth_missing_positive_number.py
@@ -0,0 +1,36 @@
+import pytest
+
+from lc_1539_kth_missing_positive_number.lc_1539_kth_missing_positive_number import (
+    Solution,
+)
+
+test_cases = (
+    # preset cases
+    ([2, 3, 4, 7, 11], 5, 9),
+    ([1, 2, 3, 4], 2, 6),
+    # common cases
+    ([2, 3, 4, 7, 11], 1, 1),
+    ([2, 3, 4, 7, 11], 2, 5),
+    ([10, 12, 13, 14], 1, 1),
+    ([10, 12, 13, 14], 2, 2),
+    ([10, 12, 13, 14], 14, 18),
+    ([10, 12, 13, 14], 11, 15),
+    ([10, 12, 13, 14], 10, 11),
+    ([10, 12, 13, 14], 9, 9),
+    # # corner cases
+    ([1], 1, 2),
+    ([2], 1, 1),
+    ([3], 1, 1),
+    ([14], 10, 10),
+    ([14], 14, 15),
+)
+
+
+@pytest.mark.parametrize(("nums", "target", "ans"), test_cases)
+def test_success_v0(nums: list[int], target: int, ans: int, solution: Solution):
+    assert ans == solution.findKthPositive(nums, target)
+
+
+@pytest.fixture
+def solution() -> Solution:
+    return Solution()
