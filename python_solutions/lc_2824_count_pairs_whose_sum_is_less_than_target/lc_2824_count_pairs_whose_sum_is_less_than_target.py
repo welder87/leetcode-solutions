e18162eeec8e90b7688bcdf23a88956d4ea67de6
welder87 <25900160+welder87@users.noreply.github.com>
2025-07-13 00:45:41 +0500

    2824. Count Pairs Whose Sum is Less than Target
    - Language: Python
    - Time complexity: O(n log n) + O(n log n)
    - Space complexity: O(n)

--- python_solutions/lc_2824_count_pairs_whose_sum_is_less_than_target/lc_2824_count_pairs_whose_sum_is_less_than_target.py
+++ python_solutions/lc_2824_count_pairs_whose_sum_is_less_than_target/lc_2824_count_pairs_whose_sum_is_less_than_target.py
@@ -9,3 +9,23 @@ class Solution:
                 if nums[i] + nums[j] < target:
                     ans += 1
         return ans
+
+    def countPairsV1(self, nums: list[int], target: int) -> int:
+        # Time complexity: O(n log n) + O(n log n). Space complexity: O(n).
+        # modified itmo
+        # [-7, -6, -2, -1, 2, 3, 5]
+        if len(nums) <= 1:
+            return 0
+        nums.sort()
+        ans = 0
+        for i in range(len(nums) - 1):
+            left, right = i + 1, len(nums)
+            while left + 1 < right:
+                mid = left + (right - left) // 2
+                if i < mid and nums[i] + nums[mid] < target:
+                    left = mid
+                else:
+                    right = mid
+            if i < left and nums[i] + nums[left] < target:
+                ans += left - i
+        return ans
--- python_solutions/lc_2824_count_pairs_whose_sum_is_less_than_target/test_lc_2824_count_pairs_whose_sum_is_less_than_target.py
+++ python_solutions/lc_2824_count_pairs_whose_sum_is_less_than_target/test_lc_2824_count_pairs_whose_sum_is_less_than_target.py
@@ -9,7 +9,15 @@ test_cases = (
     ([-1, 1, 2, 3, 1], 2, 3),
     ([-6, 2, 5, -2, -7, -1, 3], -2, 10),
     # common cases
+    ([-1, 1, 2, 0, 3, 1], 6, 15),
+    ([-1, 1, 2, 0, 3, 1], 2, 6),
+    ([-1, 1, 2, 0, 3, 1], -1, 0),
+    ([-1, -1, -1, -1], 2, 6),
+    ([-1, -1, -1, -1], -2, 0),
+    ([-1, -1, -1, -1], -3, 0),
     # corner cases
+    ([-1, -50], 3, 1),
+    ([-1, -50], -51, 0),
     ([-1], 3, 0),
     ([-1], -2, 0),
 )
@@ -18,6 +26,11 @@ test_cases = (
 @pytest.mark.parametrize(("nums", "target", "ans"), test_cases)
 def test_success_v0(nums: list[int], target: int, ans: int, solution: Solution):
     assert solution.countPairs(nums, target) == ans
+
+
+@pytest.mark.parametrize(("nums", "target", "ans"), test_cases)
+def test_success_v1(nums: list[int], target: int, ans: int, solution: Solution):
+    assert solution.countPairsV1(nums, target) == ans
 
 
 @pytest.fixture
