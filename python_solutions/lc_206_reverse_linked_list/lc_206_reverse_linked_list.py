894f39c06ab885a0ca53306074c3596ddab63b58
welder87 <25900160+welder87@users.noreply.github.com>
2025-07-28 22:15:23 +0500

    206. Reverse Linked List
    - Language: Python.
    - Time complexity: O(n).
    - Space complexity: O(1).

--- python_solutions/lc_206_reverse_linked_list/lc_206_reverse_linked_list.py
+++ python_solutions/lc_206_reverse_linked_list/lc_206_reverse_linked_list.py
@@ -14,3 +14,13 @@ class Solution:
             cur.val = lst.pop()
             cur = cur.next
         return head
+
+    def reverseListV1(self, head: ListNode | None) -> ListNode | None:
+        # Time complexity: O(n). Space complexity: O(1).
+        prev = None
+        while head is not None:
+            nxt = head.next
+            head.next = prev
+            prev = head
+            head = nxt
+        return prev
--- python_solutions/lc_206_reverse_linked_list/test_lc_206_reverse_linked_list.py
+++ python_solutions/lc_206_reverse_linked_list/test_lc_206_reverse_linked_list.py
@@ -9,19 +9,26 @@ from lc_206_reverse_linked_list.lc_206_reverse_linked_list import Solution
 
 test_cases = (
     # preset cases
-    ([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]),
-    ([1, 2], [2, 1]),
-    ([], []),
+    pytest.param([1, 2, 3, 4, 5], [5, 4, 3, 2, 1], id="0"),
+    pytest.param([1, 2], [2, 1], id="1"),
+    pytest.param([], [], id="2"),
     # common cases
-    ([-5, 0, 0, 3, -9, -9, 45], [45, -9, -9, 3, 0, 0, -5]),
+    pytest.param([-5, 0, 0, 3, -9, -9, 45], [45, -9, -9, 3, 0, 0, -5], id="3"),
     # corner cases
-    ([1], [1]),
+    pytest.param([1], [1], id="4"),
 )
 
 
 @pytest.mark.parametrize(("lst", "ans"), test_cases)
 def test_success_v0(lst: list[int], ans: list[int], solution: Solution):
     res = solution.reverseList(to_singly_linked_list(lst))
+    assert to_list(res) == ans
+    assert compare_singly_linked_lists(res, to_singly_linked_list(ans))
+
+
+@pytest.mark.parametrize(("lst", "ans"), test_cases)
+def test_success_v1(lst: list[int], ans: list[int], solution: Solution):
+    res = solution.reverseListV1(to_singly_linked_list(lst))
     assert to_list(res) == ans
     assert compare_singly_linked_lists(res, to_singly_linked_list(ans))
 
