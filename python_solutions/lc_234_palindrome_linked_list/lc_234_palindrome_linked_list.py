c41e7a3833649d821beaf11bf9be8482645fc389
welder87 <25900160+welder87@users.noreply.github.com>
2025-07-28 21:15:00 +0500

    234. Palindrome Linked List
    - Language: Python.
    - Time complexity: O(n/2 + 1 + n/2 + n/2 + 1).
    - Space complexity: O(1).

--- python_solutions/lc_234_palindrome_linked_list/lc_234_palindrome_linked_list.py
+++ python_solutions/lc_234_palindrome_linked_list/lc_234_palindrome_linked_list.py
@@ -21,3 +21,34 @@ class Solution:
             i += 1
             j -= 1
         return True
+
+    def isPalindromeV1(self, head: Optional[ListNode]) -> bool:
+        # Time complexity: O(n/2 + 1 + n/2 + n/2 + 1). Space complexity: O(1).
+        if head is None:
+            return False
+        new_head = self._reverse(self._find_mid(head))
+        return self._compare(head, new_head)
+
+    def _find_mid(self, head: ListNode) -> ListNode:
+        slow, fast = head, head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def _reverse(self, head: ListNode) -> ListNode:
+        prev = None
+        while head is not None:
+            nxt = head.next
+            head.next = prev
+            prev = head
+            head = nxt
+        return prev
+
+    def _compare(self, head1: ListNode, head2: ListNode) -> bool:
+        while head1 is not None and head2 is not None:
+            if head1.val != head2.val:
+                return False
+            head1 = head1.next
+            head2 = head2.next
+        return True
--- python_solutions/lc_234_palindrome_linked_list/test_lc_234_palindrome_linked_list.py
+++ python_solutions/lc_234_palindrome_linked_list/test_lc_234_palindrome_linked_list.py
@@ -5,23 +5,30 @@ from lc_234_palindrome_linked_list.lc_234_palindrome_linked_list import Solution
 
 test_cases = (
     # preset cases
-    ([1, 2, 2, 1], True),
-    ([1, 2], False),
+    pytest.param([1, 2, 2, 1], True, id="0"),
+    pytest.param([1, 2], False, id="2"),
     # common cases
-    ([1, 2, 1], True),
-    ([1, 2, 5, 2, 1], True),
-    ([1, 2, 8, 1], False),
-    ([1, 2, 2, 0], False),
+    pytest.param([1, 2, 1], True, id="3"),
+    pytest.param([1, 2, 5, 2, 1], True, id="4"),
+    pytest.param([1, 2, 8, 1], False, id="5"),
+    pytest.param([1, 2, 2, 0], False, id="6"),
+    pytest.param([1, 3, 2, 6, 7], False, id="7"),
+    pytest.param([1, 9, 0, 8], False, id="8"),
     # corner cases
-    ([0], True),
-    ([1, 1], True),
-    ([1, 1, 1], True),
+    pytest.param([0], True, id="9"),
+    pytest.param([1, 1], True, id="10"),
+    pytest.param([1, 1, 1], True, id="11"),
 )
 
 
 @pytest.mark.parametrize(("lst", "ans"), test_cases)
 def test_success_v0(lst: list[int], ans: bool, solution: Solution):
     assert ans is solution.isPalindrome(to_singly_linked_list(lst))
+
+
+@pytest.mark.parametrize(("lst", "ans"), test_cases)
+def test_success_v1(lst: list[int], ans: bool, solution: Solution):
+    assert ans is solution.isPalindromeV1(to_singly_linked_list(lst))
 
 
 @pytest.fixture
