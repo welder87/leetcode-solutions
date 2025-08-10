39d7b7dcced3d268b9d446a2ef1f9266dc538bc0
welder87 <25900160+welder87@users.noreply.github.com>
2025-08-10 23:17:43 +0500

    83. Remove Duplicates from Sorted List.
        - Language: Python.
        - Time complexity: O(n).
        - Space complexity: O(1).
        - Solution: https://algo.monster/liteproblems/83

--- python_solutions/lc_83_remove_duplicates_from_sorted_list/lc_83_remove_duplicates_from_sorted_list.py
+++ python_solutions/lc_83_remove_duplicates_from_sorted_list/lc_83_remove_duplicates_from_sorted_list.py
@@ -33,3 +33,20 @@ class Solution:
                 prev = cur
                 cur = cur.next
         return head
+
+    def deleteDuplicatesV2(self, head: ListNode) -> ListNode:
+        # Time complexity: O(n). Space complexity: O(1).
+        # Solution: https://algo.monster/liteproblems/83
+        # Initialize current to point to the head of the list
+        current = head
+        # Traverse the linked list
+        while current and current.next:
+            # If the current value is equal to the value in the next node
+            if current.val == current.next.val:
+                # Bypass the next node as it's a duplicate
+                current.next = current.next.next
+            else:
+                # Move to the next unique value if no duplicate is found
+                current = current.next
+        # Return the head of the updated list
+        return head
