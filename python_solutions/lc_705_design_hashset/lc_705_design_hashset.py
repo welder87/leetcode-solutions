ac1a36cb2820d703611d41f7364776456e667980
welder87 <25900160+welder87@users.noreply.github.com>
2025-08-14 22:39:48 +0500

    705. Design HashSet. Stub.

--- python_solutions/lc_705_design_hashset/lc_705_design_hashset.py
+++ python_solutions/lc_705_design_hashset/lc_705_design_hashset.py
@@ -4,10 +4,12 @@ from common_datastructures import ListNode
 class MyHashSet:
     def __init__(self):
         self._size = 4
+        self._power = 2
         self._capacity = 0
         self._chains = [None] * self._size
 
     def add(self, key: int) -> None:
+        self._check_resize()
         idx = self._cals_idx(key)
         if self._chains[idx] is None:
             self._chains[idx] = ListNode(val=key)
@@ -48,3 +50,21 @@ class MyHashSet:
 
     def _cals_idx(self, key: int) -> int:
         return hash(key) % self._size
+
+    def _check_resize(self):
+        if 3 * self._capacity > 2 * self._size:
+            self._resize()
+
+    def _resize(self):
+        self._capacity = 0
+        self._power += 1
+        self._size = 2**self._power
+        old_chains = self._chains
+        self._chains = [None] * self._size
+        for idx in range(len(old_chains)):
+            node = old_chains[idx]
+            old_chains[idx] = None
+            while node is not None:
+                self.add(node.val)
+                node = node.next
+        old_chains = None
