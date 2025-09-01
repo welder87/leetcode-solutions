bf7c2803b371e239ead1e5dfca839df5167da2ff
welder87 <25900160+welder87@users.noreply.github.com>
2025-09-02 00:06:37 +0500

    1299. Replace Elements with Greatest Element on Right Side.     - Language: Python.     - Time complexity: O(n).     - Space complexity: O(1).

--- python_solutions/lc_1299_replace_elements_with_gt_element_on_right/lc_1299_replace_elements_with_gt_element_on_right.py
+++ python_solutions/lc_1299_replace_elements_with_gt_element_on_right/lc_1299_replace_elements_with_gt_element_on_right.py
@@ -10,3 +10,13 @@ class Solution:
             arr[i] = arr[mx]
         arr[-1] = -1
         return arr
+
+    def replaceElementsV1(self, arr: list[int]) -> list[int]:
+        # Time complexity: O(n). Space complexity: O(1).
+        mx = -1
+        for i in range(-1, -(len(arr) + 1), -1):
+            if arr[i] > mx:
+                arr[i], mx = mx, arr[i]
+            else:
+                arr[i] = mx
+        return arr
--- python_solutions/lc_1299_replace_elements_with_gt_element_on_right/test_lc_1299_replace_elements_with_gt_element_on_right.py
+++ python_solutions/lc_1299_replace_elements_with_gt_element_on_right/test_lc_1299_replace_elements_with_gt_element_on_right.py
@@ -10234,6 +10234,11 @@ def test_success_v0(nums: list[int], ans: list[int], solution: Solution):
     assert solution.replaceElements(nums) == ans
 
 
+@pytest.mark.parametrize(("nums", "ans"), test_cases)
+def test_success_v1(nums: list[int], ans: list[int], solution: Solution):
+    assert solution.replaceElementsV1(nums) == ans
+
+
 @pytest.fixture
 def solution() -> Solution:
     return Solution()
