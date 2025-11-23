8401b5b111d4a5a6f007cc53a18efb02cf1009df
welder87 <25900160+welder87@users.noreply.github.com>
2025-11-24 00:31:39 +0500

    283. Move Zeroes.     - Language: Golang.     - Time complexity: O(n).     - Space complexity: O(1).

--- golang_solutions/283_move_zeroes/main.go
+++ golang_solutions/283_move_zeroes/main.go
@@ -1,5 +1,6 @@
 package problem283
 
+// Time complexity: O(n). Space complexity: O(1).
 func moveZeroes(nums []int) {
 	if len(nums) <= 1 {
 		return
@@ -19,5 +20,19 @@ func moveZeroes(nums []int) {
 		} else {
 			i++
 		}
+	}
+}
+
+// Time complexity: O(n). Space complexity: O(1).
+func moveZeroesV1(nums []int) {
+	i, j := 0, 1
+	for j < len(nums) {
+		if nums[i] == 0 && nums[j] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i += 1
+		} else if nums[i] != 0 {
+			i += 1
+		}
+		j += 1
 	}
 }
--- golang_solutions/283_move_zeroes/main_test.go
+++ golang_solutions/283_move_zeroes/main_test.go
@@ -1,32 +1,63 @@
 package problem283
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestMoveZeroes(t *testing.T) {
+	testMoveZeroes(t, moveZeroes)
+}
+
+func TestMoveZeroesV1(t *testing.T) {
+	testMoveZeroes(t, moveZeroesV1)
+}
+
+func testMoveZeroes(t *testing.T, fn func([]int)) {
 	testCases := []struct {
-		nums []int
-		ans  []int
+		testName string
+		nums     []int
+		ans      []int
 	}{
-		{[]int{0, 1, 0, 3, 12}, []int{1, 3, 12, 0, 0}},
-		{[]int{0}, []int{0}},
-		{[]int{}, []int{}},
-		{[]int{1, 3, 12, 0, 0}, []int{1, 3, 12, 0, 0}},
-		{[]int{0, 1, 15, 0, 12, 3, 15}, []int{1, 15, 12, 3, 15, 0, 0}},
+		// PRESET CASES
+		{"Preset_case_1", []int{0, 1, 0, 3, 12}, []int{1, 3, 12, 0, 0}},
+		{"Preset_case_2", []int{0}, []int{0}},
+		// COMMON CASES
+		{"Trailing_zeros", []int{1, 3, 12, 0, 0}, []int{1, 3, 12, 0, 0}},
+		{"Leading_zeros", []int{0, 0, 0, 1, 2, 3}, []int{1, 2, 3, 0, 0, 0}},
+		{"Embedded_zeros", []int{1, 0, 2, 0, 3, 0}, []int{1, 2, 3, 0, 0, 0}},
 		{
-			[]int{0, 0, 0, -15, 10, -1, 0, 12, 0, 3, 15},
-			[]int{-15, 10, -1, 12, 3, 15, 0, 0, 0, 0, 0},
+			"Alternating_zeros_and_non_zeros_(zero_start)",
+			[]int{0, 1, 0, 2, 0, 3, 0, 4},
+			[]int{1, 2, 3, 4, 0, 0, 0, 0},
 		},
+		{
+			"Alternating_zeros and non_zeros (non_zero_start)",
+			[]int{1, 0, 2, 0, 3, 0, 4},
+			[]int{1, 2, 3, 4, 0, 0, 0},
+		},
+		{
+			"Leading_and_trailing_zeros",
+			[]int{0, 0, 0, 1, 3, 12, 0, 0},
+			[]int{1, 3, 12, 0, 0, 0, 0, 0},
+		},
+		{
+			"Large_numbers",
+			[]int{1000000, 0, 2147483648, 0, 0, 42},
+			[]int{1000000, 2147483648, 42, 0, 0, 0},
+		},
+		// CORNER CASES
+		{"No_zeros", []int{1, 6, 1, 24}, []int{1, 6, 1, 24}},
+		{"All_zeros", []int{0, 0, 0}, []int{0, 0, 0}},
+		{"Single_Zero", []int{0}, []int{0}},
+		{"Single_Non_Zero", []int{5}, []int{5}},
 	}
-	for _, testCase := range testCases {
-		testName := fmt.Sprintf("%v", testCase.nums)
-		t.Run(testName, func(t *testing.T) {
-			moveZeroes(testCase.nums)
-			if !reflect.DeepEqual(testCase.nums, testCase.ans) {
-				t.Errorf("got %v, want %v", testCase.nums, testCase.ans)
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			fn(tc.nums)
+			if !reflect.DeepEqual(tc.nums, tc.ans) {
+				t.Errorf("got %v, want %v", tc.nums, tc.ans)
 			}
 		})
 	}
