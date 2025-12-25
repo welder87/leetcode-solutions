95d77e333f3963462eaedf0d1e2f32f42183e02d
welder87 <25900160+welder87@users.noreply.github.com>
2025-12-25 23:03:29 +0500

    56. Merge Intervals.     - Language: Golang.     - Time complexity: O(n log n). Space complexity: O(n log n).

---
+++ golang_solutions/56_merge_intervals/main.go
@@ -0,0 +1,25 @@
+package problem56
+
+import "sort"
+
+// Time complexity: O(n log n). Space complexity: O(n log n).
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	ans := make([][]int, 0, len(intervals))
+	intermediate := []int{intervals[0][0], intervals[0][1]}
+	for i := 1; i < len(intervals); i++ {
+		start := max(intermediate[0], intervals[i][0])
+		end := min(intermediate[1], intervals[i][1])
+		if start <= end {
+			intermediate[0] = min(intermediate[0], intervals[i][0])
+			intermediate[1] = max(intermediate[1], intervals[i][1])
+		} else {
+			ans = append(ans, intermediate)
+			intermediate = []int{intervals[i][0], intervals[i][1]}
+		}
+	}
+	ans = append(ans, intermediate)
+	return ans
+}
---
+++ golang_solutions/56_merge_intervals/main_test.go
@@ -0,0 +1,88 @@
+package problem56
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	testMerge(t, merge)
+}
+
+func testMerge(t *testing.T, fn func([][]int) [][]int) {
+	testCases := []struct {
+		name      string
+		intervals [][]int
+		ans       [][]int
+	}{
+		{
+			"Preset case 1 (overlapping intervals)",
+			[][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}},
+			[][]int{{1, 6}, {8, 10}, {15, 18}},
+		},
+		{
+			"Preset case 2 (adjacent intervals)",
+			[][]int{{1, 4}, {4, 5}},
+			[][]int{{1, 5}},
+		},
+		{
+			"Preset case 3 (unsorted overlapping)",
+			[][]int{{4, 7}, {1, 4}},
+			[][]int{{1, 7}},
+		},
+		{
+			"Sorted overlapping",
+			[][]int{{1, 1}, {2, 3}, {2, 6}, {2, 15}},
+			[][]int{{1, 1}, {2, 15}},
+		},
+		{
+			"Single interval",
+			[][]int{{4, 7}},
+			[][]int{{4, 7}},
+		},
+		{
+			"No overlaps",
+			[][]int{{1, 2}, {3, 6}, {7, 15}},
+			[][]int{{1, 2}, {3, 6}, {7, 15}},
+		},
+		{
+			"All overlapping (first)",
+			[][]int{{1, 4}, {2, 3}, {0, 5}},
+			[][]int{{0, 5}},
+		},
+		{
+			"All overlapping (second)",
+			[][]int{{1, 3}, {2, 6}, {5, 15}, {15, 18}},
+			[][]int{{1, 18}},
+		},
+		{
+			"Unsorted nested intervals (first)",
+			[][]int{{1, 4}, {3, 5}, {2, 4}, {7, 10}},
+			[][]int{{1, 5}, {7, 10}},
+		},
+		{
+			"Unsorted nested intervals (second)",
+			[][]int{{7, 15}, {1, 1}, {1, 6}, {15, 15}},
+			[][]int{{1, 6}, {7, 15}},
+		},
+		{
+			"Touching intervals multiple",
+			[][]int{{1, 2}, {2, 3}, {3, 4}},
+			[][]int{{1, 4}},
+		},
+		{
+			"Unsorted with gaps",
+			[][]int{{2, 3}, {2, 2}, {3, 3}, {1, 3}, {5, 7}, {2, 2}, {4, 6}},
+			[][]int{{1, 3}, {4, 7}},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			ans := fn(testCase.intervals)
+
+			if !reflect.DeepEqual(ans, testCase.ans) {
+				t.Errorf("got %v, ans %v", ans, testCase.ans)
+			}
+		})
+	}
+}
