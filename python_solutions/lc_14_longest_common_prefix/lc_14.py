b9f99e8a7b78a24954e75f2cd7c2769f04cdc3f5
welder87 <25900160+welder87@users.noreply.github.com>
2026-01-30 00:24:25 +0500

    14. Longest Common Prefix.     - Language: Python.     - Time complexity: O(sum(char)).     - Space complexity: O(1).

--- python_solutions/lc_14_longest_common_prefix/lc_14.py
+++ python_solutions/lc_14_longest_common_prefix/lc_14.py
@@ -1,3 +1,6 @@
+from itertools import zip_longest
+
+
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
         # Time complexity: O(sum(char)). Space complexity: O(k).
@@ -12,3 +15,15 @@ class Solution:
                 break
             prefix = f"{prefix}{r.pop()}"
         return prefix
+
+    def longestCommonPrefixV1(self, strs: list[str]) -> str:
+        # Time complexity: O(sum(char)). Space complexity: O(1).
+        if len(strs) == 1:
+            return strs[0]
+        ans = list(strs[0])
+        for i in range(1, len(strs)):
+            for j, (first, second) in enumerate(zip_longest(strs[i], ans)):
+                if first != second:
+                    ans = ans[:j]
+                    break
+        return "".join(ans)
--- python_solutions/lc_14_longest_common_prefix/test_lc_14.py
+++ python_solutions/lc_14_longest_common_prefix/test_lc_14.py
@@ -47,6 +47,11 @@ def test_success_v0(strs: list[str], ans: str, solution: Solution):
     assert ans == solution.longestCommonPrefix(strs)
 
 
+@pytest.mark.parametrize(("strs", "ans"), test_cases)
+def test_success_v1(strs: list[str], ans: str, solution: Solution):
+    assert ans == solution.longestCommonPrefix(strs)
+
+
 @pytest.fixture
 def solution() -> Solution:
     return Solution()
